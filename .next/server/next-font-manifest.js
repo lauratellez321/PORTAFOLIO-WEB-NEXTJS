14ee21906a38d0f46616d4f6a9e4c61835d0989c
lauratellez321 <lauravalentc@gmail.com>
2024-08-29 22:29:59 -0500

    Project Setup

---
+++ .next/server/next-font-manifest.js
@@ -0,0 +1,1 @@
+self.__NEXT_FONT_MANIFEST="{\"pages\":{},\"app\":{\"/Users/valentina/Documents/Develop/PORTAFOLIO-WEB-NEXTJS/app/layout\":[\"static/media/a34f9d1faa5f3315-s.p.woff2\"]},\"appUsingSizeAdjust\":true,\"pagesUsingSizeAdjust\":false}"
